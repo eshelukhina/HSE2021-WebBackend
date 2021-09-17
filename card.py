b304ffa712bcd075e2f103e41e9e52253eb19c63
Ekaterina Shelukhina <shelukchina2447@gmail.com>
2021-09-17 19:57:51 +0300

    task 1

---
+++ card.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel
+
+
+class MedicalCard(BaseModel):
+    name: str
+    age: int
+    gender: str
+    diagnosis: str
+
+
+def is_valid_name(name):
+    word_list = name.split()
+    number_of_words = len(word_list)
+    if number_of_words == 2:
+        return True
+    return False
+
+
+def is_valid_age(age):
+    if age >= 0:
+        return True
+    return False
+
+
+def is_valid_patient(card: MedicalCard):
+    if is_valid_name(card.name) and is_valid_age(card.age):
+        return True
+    return False
